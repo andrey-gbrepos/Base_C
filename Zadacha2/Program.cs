b3666beb0748babb520bbc2ec0e2e903fce3e3c8
andrey-gbrepose <gandire68@gmail.com>
2023-06-23 10:51:49 +0300

    Три задачи семниара1 C#

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,43 @@
+﻿Console.Clear();
+Console.Write("Введите  число: ");
+int i = int.Parse(Console.ReadLine());
+if (i < 1 || i > 7)
+    {
+       Console.Write("Введен неправильный номер");
+       return;
+    }
+if (i == 1)
+      {
+        Console.WriteLine("Понедельник");
+        return;
+}
+if (i == 2)
+      {
+        Console.WriteLine("Второник");
+        return;
+}
+if (i == 3)
+      {
+        Console.WriteLine("Среда");
+        return;
+}
+if (i == 4)
+      {
+        Console.WriteLine("Четверг");
+        return;
+}
+if (i == 5)
+      {
+        Console.WriteLine("Пятница");
+        return;
+}
+if (i == 6)
+      {
+        Console.WriteLine("Суббота");
+        return;
+}
+if (i == 7)
+      {
+        Console.WriteLine("Воскресение");
+        return;
+}
---
+++ Zadacha3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе 
+//показывает все целые числа в диапазоне -N  до N
+Console.Clear();
+Console.Write("Введите  число: ");
+int i = int.Parse(Console.ReadLine());
+int n = -i;
+while  (n<=i)
+{
+    Console.WriteLine(n);
+    n++;
+}
